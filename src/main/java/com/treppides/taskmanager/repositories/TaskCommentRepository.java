37cf8d42afedf0b12fd496f4c92bfb0c2d5c052b
lygia-p <lygiapampaka@yahoo.com>
2026-06-03 10:32:53 +0300

    Delete src/main directory

--- src/main/java/com/treppides/taskmanager/repositories/TaskCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.treppides.taskmanager.repositories;
-
-import com.treppides.taskmanager.entities.TaskComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TaskCommentRepository extends JpaRepository<TaskComment, Integer> {
-    List<TaskComment> findByTask_TaskIdOrderByCreatedAtAsc(Integer taskId);
-}
--- src/main/java/com/treppides/taskmanager/repositories/TaskHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.treppides.taskmanager.repositories;
-
-import com.treppides.taskmanager.entities.TaskHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TaskHistoryRepository extends JpaRepository<TaskHistory, Integer> {
-    List<TaskHistory> findByTask_TaskIdOrderByChangedAtAsc(Integer taskId);
-}
